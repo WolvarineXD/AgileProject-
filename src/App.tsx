0796d057aa9cfd678888e072f34a3225a8f1e832
WolvarineXD <148878451+WolvarineXD@users.noreply.github.com>
2025-04-17 22:36:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />   {/* Home Page */}
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/digital-goods/:id" element={<DigitalGoodDetail />} />  {/* Dynamic Route */}
-          <Route path="*" element={<NotFound />} /> {/* 404 Page */}
-        </Routes>
-      </BrowserRouter>
+      
     </TooltipProvider>
   </QueryClientProvider>
 );
