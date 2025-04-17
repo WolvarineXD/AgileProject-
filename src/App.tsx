cb03eee41b0ca046c67e2bb7a27a769ccc4d4d7e
WolvarineXD <148878451+WolvarineXD@users.noreply.github.com>
2025-04-17 22:41:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter,Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -16,7 +17,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />   {/* Home Page */}
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/digital-goods/:id" element={<DigitalGoodDetail />} />  {/* Dynamic Route */}
+          <Route path="*" element={<NotFound />} /> {/* 404 Page */}
+        </Routes>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
